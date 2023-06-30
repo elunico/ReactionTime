88b1d2a99bf9ab90adb99f469d14e825e01cff02
Thomas Povinelli <elunico@users.noreply.github.com>
2023-06-30 01:05:17 -0400

    Changed project to have modules because idk java hates me i guess

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module reactiontime_kt {
+    requires kotlin.stdlib;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    exports reactiontime_kt to javafx.graphics;
+}
